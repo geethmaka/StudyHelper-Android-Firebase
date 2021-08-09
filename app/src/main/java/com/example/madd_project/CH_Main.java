f0f045a7ccf65dbf8899e51f623123ac613d6481
Nipun Senarath <smdnipun@gmail.com>
2021-08-09 11:57:13 +0530

    Editted common UIs

--- app/src/main/java/com/example/madd_project/CH_Main.java
+++ app/src/main/java/com/example/madd_project/CH_Main.java
@@ -3,12 +3,14 @@ package com.example.madd_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class CH_Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_ch_main);
     }
 }
