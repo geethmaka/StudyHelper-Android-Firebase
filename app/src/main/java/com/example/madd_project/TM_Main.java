3c8a2facd946c5f24ba116ce385050c1e03d0338
pamudithaKekulandara <pkekulandera@gmail.com>
2021-08-12 16:22:06 +0530

    added background colors

--- app/src/main/java/com/example/madd_project/TM_Main.java
+++ app/src/main/java/com/example/madd_project/TM_Main.java
@@ -2,7 +2,10 @@ package com.example.madd_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TM_Main extends AppCompatActivity {
 
@@ -11,6 +14,23 @@ public class TM_Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tm_main);
 
+    }
+    //Navigation between buttons
+    public void gotoPdf(View view){
+        final Context context = this;
+        Intent intent = new Intent(context,tm_pdf.class);
+        startActivity(intent);
+
+    }
+    public void gotoLink(View view){
+        final Context context = this;
+        Intent intent = new Intent(context,tm_link.class);
+        startActivity(intent);
+    }
+    public void gotoComplain(View view){
+        final Context context = this;
+        Intent intent = new Intent(context,tm_complain.class);
+        startActivity(intent);
 
     }
 }
