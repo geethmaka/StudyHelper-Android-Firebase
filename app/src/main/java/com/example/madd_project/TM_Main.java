65a5daeb01f7636a158edfe401be143037029540
pamudithaKekulandara <pkekulandera@gmail.com>
2021-08-13 22:40:13 +0530

    updated

--- app/src/main/java/com/example/madd_project/TM_Main.java
+++ app/src/main/java/com/example/madd_project/TM_Main.java
@@ -46,8 +46,8 @@ public class TM_Main extends AppCompatActivity {
         View popupView = inflater.inflate(R.layout.pdf_popup_window, null);
 
         // create the popup window
-        int width = LinearLayout.LayoutParams.MATCH_PARENT;
-        int height = LinearLayout.LayoutParams.MATCH_PARENT;
+        int width = LinearLayout.LayoutParams.WRAP_CONTENT;
+        int height = LinearLayout.LayoutParams.WRAP_CONTENT;
         boolean focusable = true; // lets taps outside the popup also dismiss it
         final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
 
