e0619b99936a421849657f52c8e5d67741cfbaca
GeethOnion <geethmaka@gmail.com>
2021-08-13 12:29:59 +0530

    Refactor: edited pages and renamed some pages

--- app/src/main/java/com/example/madd_project/CM_Main.java
+++ app/src/main/java/com/example/madd_project/CM_Main.java
@@ -9,6 +9,6 @@ public class CM_Main extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cm_main);
+        setContentView(R.layout.course_management);
     }
 }
