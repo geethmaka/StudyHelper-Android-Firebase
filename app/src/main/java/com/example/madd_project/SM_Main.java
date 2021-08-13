a356a0fede6a78a9e1fa1d1cc3c68d16c02468b5
Hiroshac <hiroshachandramal01@gmail.com>
2021-08-14 04:39:11 +0530

    Create edit page,rq page,complain age.

--- app/src/main/java/com/example/madd_project/SM_Main.java
+++ app/src/main/java/com/example/madd_project/SM_Main.java
@@ -2,7 +2,10 @@ package com.example.madd_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SM_Main extends AppCompatActivity {
 
@@ -11,4 +14,20 @@ public class SM_Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sm_main);
     }
+    public void gotosmcompalin(View view) {
+        final Context context = this;
+        Intent intent = new Intent(context, sm_complain.class);
+        startActivity(intent);
+    }
+    public void gotoedit(View view) {
+        final Context context = this;
+        Intent intent = new Intent(context, sm_edit.class);
+        startActivity(intent);
+    }
+    public void gotorequestquestion(View view) {
+        final Context context = this;
+        Intent intent = new Intent(context, sm_rq.class);
+        startActivity(intent);
+    }
+
 }
