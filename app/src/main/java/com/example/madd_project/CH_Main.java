6af8eb207d92d03d75a5d90e0fbcb3208c0394a7
Nipun Senarath <smdnipun@gmail.com>
2021-08-14 00:28:55 +0530

    Created the navigation bar

--- app/src/main/java/com/example/madd_project/CH_Main.java
+++ app/src/main/java/com/example/madd_project/CH_Main.java
@@ -1,16 +1,28 @@
 package com.example.madd_project;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class CH_Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_ch_main);
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.chbottomNav);
+        NavController navController = Navigation.findNavController(this,R.id.fragmentContainerView);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+//        NavigationUI.setupActionBarWithNavController(this, navController);
+
+
+//        String appBarConfig = AppBarConfiguration(setOf(R.id.ch_DashboardFragment,R.id.ch_ComplainFragment,R.id.ch_UserDetailsFragment,R.id.ch_ReportFragment));
     }
 }
--- app/src/main/java/com/example/madd_project/Registration.java
+++ app/src/main/java/com/example/madd_project/Registration.java
@@ -9,6 +9,6 @@ public class Registration extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_registration);
+        setContentView(R.layout.registration);
     }
 }
