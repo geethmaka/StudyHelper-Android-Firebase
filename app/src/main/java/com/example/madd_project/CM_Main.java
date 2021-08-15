05e5b4b31034a236a6c6931d069f33d266fe2ee5
GeethOnion <geethmaka@gmail.com>
2021-08-15 11:33:44 +0530

    ui:added search ui

--- app/src/main/java/com/example/madd_project/CM_Main.java
+++ app/src/main/java/com/example/madd_project/CM_Main.java
@@ -6,10 +6,13 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
