400ac7a48575d2861f8a85f69f610f2f950d20c9
Nipun Senarath <smdnipun@gmail.com>
2021-09-18 21:27:01 +0530

    Regenerated the files

--- app/src/main/java/com/example/studyhelper_android_firebase/Login.java
+++ app/src/main/java/com/example/studyhelper_android_firebase/Login.java
@@ -2,7 +2,10 @@ package com.example.studyhelper_android_firebase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void gotoCHProfile(View view) {
+        final Context context = this;
+        Intent intent = new Intent(context, ComplainMain.class);
+        startActivity(intent);
     }
 }
