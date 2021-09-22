bc34e22e2037bb61b7bc04bdeee4fb6b6a9213cc
pamudithaKekulandara <pkekulandera@gmail.com>
2021-09-22 23:08:50 +0530

    complain java

---
+++ app/src/main/java/com/example/studyhelper_android_firebase/Added_complain_T.java
@@ -0,0 +1,14 @@
+package com.example.studyhelper_android_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Added_complain_T extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.added_complain_t);
+    }
+}
