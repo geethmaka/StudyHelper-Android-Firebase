cab53adf7e8306d4f00f694d569ab2eb26b5c909
Hiroshac <hiroshachandramal01@gmail.com>
2021-10-07 01:22:33 +0530

    create test case

---
+++ app/src/androidTest/java/com/example/studyhelper_android_firebase/student/Student_complaintTest.java
@@ -0,0 +1,17 @@
+package com.example.studyhelper_android_firebase.student;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class Student_complaintTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
